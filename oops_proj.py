17199ef7313e123d272a5a6a75aab986ba1ba51b
Arjun <arjunmehrania@gmail.com>
2026-02-20 22:48:25 +0530

    3-- covered till chatbook project

--- oops_proj.py
+++ oops_proj.py
@@ -18,9 +18,9 @@ class chatbook:
         elif user_input=='2':
             self.signin()
         elif user_input == '3':
-            pass
+            self.mypost()
         elif user_input== '4':
-            pass
+            self.msgfrnd()
         else:
             exit()
 
@@ -40,8 +40,24 @@ class chatbook:
             pwd=input("enter your password here ")
             if self.usrname==uname and self.passkey==pwd :
                 print("User signin successful")
+                self.loggedin=True
             else:
                 print("Invalid credentials")
         self.menu()
+    def mypost(self):
+        if self.loggedin==True:
+            txt=input("Write your post: ")
+            print("Posted successfully")
+        else:
+            print("Sign in first! \n")
+        self.menu()
 
-object1= chatbook()
+    def msgfrnd(self):
+        if self.loggedin==True:
+            msg=input("Type your message here: ")
+            frnd=input("userid of your friend: ")
+            print(f"Message sent to {frnd} \n")
+        else:
+            print("Signin First! \n")
+        self.menu()            
+#object1= chatbook()
